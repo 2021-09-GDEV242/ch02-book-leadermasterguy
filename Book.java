9f9fdd70fde738421fe6d7f388203f4bd752eff7
leadermasterguy <leadermasterguy2@gmail.com>
2021-09-21 00:13:00 -0400

    2.88 refNumber
    added the field refNumber, along with methods to set, get, and print
    "" until set
    implemented into printDetails

--- Book.java
+++ Book.java
@@ -1,7 +1,8 @@
 /**
  * A class that maintains information on a book, including author, title,
  * and the number of pages.
- * implemented methods to get, and print fields
+ * allows for the addition of a reference number via set method
+ * implemented methods to get, and print all fields
  * printDetails prints all fields simultaneously
  * @author Nicholas Trilone
  * @version 2021.09.20
@@ -11,17 +12,22 @@ class Book
     // The fields.
     private String author;
     private String title;
+    //added 2.85
     private int pages;
+    //added 2.88
+    private String refNumber;
     
     /**
-     * Set the author and title fields when this object
+     * Set the author, title, pages, and ref fields when this object
      * is constructed.
+     * modified for 2.85 and 2.88
      */
     public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
+        refNumber = "";
     }
 
     /**
@@ -52,6 +58,15 @@ class Book
     }
     
     /**
+     * Returns reference number
+     * for 2.88
+     */
+    public String getRefNumber()
+    {
+        return refNumber;   
+    }
+    
+    /**
      * Prints book title
      * for 2.84
      */
@@ -79,13 +94,41 @@ class Book
     }
     
     /**
+     * Prints reference number
+     * for 2.88
+     */
+    public void printRefNumber()
+    {
+        //prints reference number if set, says that it's not set if it's not set
+        if(refNumber!=""){
+           System.out.println("This book's reference number is "+refNumber+".");  
+        }
+        else{
+           System.out.println("This book's reference number has not been set.");
+        }
+    }
+    
+    public void setRefNumber(String ref)
+    {
+        refNumber = ref;
+    }
+    
+    /**
      * Prints a formatted list of book qualities
      * for 2.87
+     * modified for 2.88
      */
     public void printDetails()
     {
         System.out.println("Title: "+title);
         System.out.println("Author: "+author);
-        System.out.println("Number of Pages:"+pages);
+        System.out.println("Number of Pages: "+pages);
+        //if there is no reference number assigned, it will say so
+        if(refNumber!=""){
+           System.out.println("Reference Number: "+refNumber);  
+        }
+        else{
+           System.out.println("Reference Number: N/A");   
+        }
     }
 }
